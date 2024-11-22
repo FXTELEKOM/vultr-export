f4858df81c708b121d97d73073f014f8847b45c6
libDarkstreet <tkristof@protonmail.ch>
2024-11-22 10:56:37 +0100

    GB added

--- export.py
+++ export.py
@@ -5,7 +5,7 @@ url = 'https://geofeed.constant.com/'
 
 output_file = 'ips.txt'
 
-country_codes = {"NL", "DE", "PL"}
+country_codes = {"NL", "DE", "PL", "GB"}
 
 headers = {
     'User-Agent': 'FXtelekom IP Crawler (Contact: contact@fxtelekom.org)',
